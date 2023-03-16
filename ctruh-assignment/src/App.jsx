25f7214b69ad0669d632829d1ab18753bcefafab
TanveerAli345 <tanveerali345@gmail.com>
2023-03-16 17:07:17 +0530

    Adding two digit number input functionality

--- ctruh-assignment/src/App.jsx
+++ ctruh-assignment/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import './App.css'
 import Canvas from './components/Canvas'
 import Tools from './components/Tools'
@@ -6,6 +6,8 @@ import Tools from './components/Tools'
 function App() {
   const [color, setColor] = useState("#ffffff")
   const [colorPalette, setColorPalette] = useState("")
+  const canvasRef = useRef(null)
+  const [number, setNumber] = useState(69)
 
   const handleColorChange = (e) => {
     setColor(e.target.value);
@@ -14,14 +16,21 @@ function App() {
   const handleColorPaletteChange = (e) => {
     setColorPalette(e.target.value);
   }
+
+  const handleNumberChange = (e) => {
+    setNumber(e.target.value)
+    console.log(number)
+  }
   
   return (
     <div className="main" style={{background: colorPalette}}>
       <Tools
         handleColorChange = {handleColorChange}
         handleColorPaletteChange = {handleColorPaletteChange}
+        handleNumberChange = {handleNumberChange}
       />
       <Canvas
+        canvasRef = {canvasRef}
         color = {color}
       />
     </div>
--- ctruh-assignment/src/components/Tools.jsx
+++ ctruh-assignment/src/components/Tools.jsx
@@ -1,12 +1,12 @@
 import React from "react"
 import colorPalette from '../data/colorPalettes.json'
 
-export default function Tools({ handleColorChange, handleColorPaletteChange }) {
+export default function Tools({ handleColorChange, handleColorPaletteChange, handleNumberChange }) {
     return(
         <div className="tools">
             <div className="canvas-tools">
                 <input id="ip-color" type="color" onChange={handleColorChange} />
-                <input id="ip-number" type="number" />
+                <input id="ip-number" type="number" onChange={handleNumberChange} />
             </div>
             
             <select id="ip-select" onChange={handleColorPaletteChange}>
